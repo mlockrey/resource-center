c13ba738650ae0e877a124151f3240dc74206bc6
Michael Lockrey <michaellockrey@MacBook-Pro.gateway>
2017-07-29 22:12:55 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './Portrait_MLL.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </div>
+        <h2>Team Yerkcol</h2>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
